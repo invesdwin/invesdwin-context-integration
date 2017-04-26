7b1c0b3b705ad81f7cbc8931c2486aa49d0e727d
subes <gsubes@gmail.com>
2017-04-26 15:01:56 +0200

    support for multiple repos

--- invesdwin-context-integration-parent/invesdwin-context-integration-maven/src/main/java/de/invesdwin/context/integration/maven/MavenProperties.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-maven/src/main/java/de/invesdwin/context/integration/maven/MavenProperties.java
@@ -1,16 +1,14 @@
 package de.invesdwin.context.integration.maven;
 
 import java.io.File;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.concurrent.Immutable;
 
 import org.sonatype.aether.repository.Authentication;
 import org.sonatype.aether.repository.RemoteRepository;
-
-import com.jcabi.aether.Aether;
 
 import de.invesdwin.context.system.properties.SystemProperties;
 import de.invesdwin.util.lang.Strings;
@@ -19,46 +17,45 @@ import de.invesdwin.util.lang.Strings;
 public final class MavenProperties {
 
     public static final File LOCAL_REPOSITORY_DIRECTORY;
-    public static final URL DEFAULT_REMOTE_REPOSITORY_URL;
-    public static final String DEFAULT_REMOTE_REPOSITORY_USERNAME;
-    public static final String DEFAULT_REMOTE_REPOSITORY_PASSWORD;
+    public static final List<RemoteRepository> REMOTE_REPOSITORIES;
 
     static {
         final SystemProperties systemProperties = new SystemProperties(MavenProperties.class);
         LOCAL_REPOSITORY_DIRECTORY = systemProperties.getFile("LOCAL_REPOSITORY_DIRECTORY");
-        DEFAULT_REMOTE_REPOSITORY_URL = systemProperties.getURL("DEFAULT_REMOTE_REPOSITORY_URL", false);
-        final String usernameKey = "DEFAULT_REMOTE_REPOSITORY_USERNAME";
-        if (systemProperties.containsValue(usernameKey)) {
-            DEFAULT_REMOTE_REPOSITORY_USERNAME = systemProperties.getString(usernameKey);
-        } else {
-            DEFAULT_REMOTE_REPOSITORY_USERNAME = null;
+
+        final List<RemoteRepository> remoteRepositories = new ArrayList<RemoteRepository>();
+        int index = 1;
+        while (true) {
+            final String urlKey = "REMOTE_REPOSITORY_" + index + "_URL";
+            if (!systemProperties.containsValue(urlKey)) {
+                break;
+            }
+            final String url = systemProperties.getString(urlKey);
+
+            final String usernameKey = "REMOTE_REPOSITORY_" + index + "_USERNAME";
+            final String username;
+            if (systemProperties.containsValue(usernameKey)) {
+                username = systemProperties.getString(usernameKey);
+            } else {
+                username = null;
+            }
+            final String passwordKey = "REMOTE_REPOSITORY_" + index + "_PASSWORD";
+            final String password;
+            if (systemProperties.containsValue(passwordKey)) {
+                password = systemProperties.getString(passwordKey);
+            } else {
+                password = null;
+            }
+            final RemoteRepository repo = new RemoteRepository(String.valueOf(index), "default", url);
+            if (Strings.isNotBlank(username) || Strings.isNotBlank(password)) {
+                repo.setAuthentication(new Authentication(username, password));
+            }
+            remoteRepositories.add(repo);
+            index++;
         }
-        final String passwordKey = "DEFAULT_REMOTE_REPOSITORY_PASSWORD";
-        if (systemProperties.containsValue(passwordKey)) {
-            DEFAULT_REMOTE_REPOSITORY_PASSWORD = systemProperties.getString(passwordKey);
-        } else {
-            DEFAULT_REMOTE_REPOSITORY_PASSWORD = null;
-        }
+        REMOTE_REPOSITORIES = Collections.unmodifiableList(remoteRepositories);
     }
 
     private MavenProperties() {}
-
-    public static Authentication newDefaultAuthentication() {
-        if (Strings.isNotBlank(DEFAULT_REMOTE_REPOSITORY_USERNAME)
-                || Strings.isNotBlank(DEFAULT_REMOTE_REPOSITORY_PASSWORD)) {
-            return new Authentication(DEFAULT_REMOTE_REPOSITORY_USERNAME, DEFAULT_REMOTE_REPOSITORY_PASSWORD);
-        } else {
-            return null;
-        }
-    }
-
-    public static Aether newDefaultAether() {
-        final RemoteRepository remoteRepository = new RemoteRepository("default", "default",
-                DEFAULT_REMOTE_REPOSITORY_URL.toString());
-        remoteRepository.setAuthentication(newDefaultAuthentication());
-        final Collection<RemoteRepository> remotes = Arrays.asList(remoteRepository);
-        final Aether aether = new Aether(remotes, LOCAL_REPOSITORY_DIRECTORY);
-        return aether;
-    }
 
 }
--- invesdwin-context-integration-parent/invesdwin-context-integration-maven/src/test/java/de/invesdwin/context/integration/maven/MavenPropertiesTest.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-maven/src/test/java/de/invesdwin/context/integration/maven/MavenPropertiesTest.java
@@ -9,18 +9,24 @@ import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.resolution.DependencyResolutionException;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
 
+import com.jcabi.aether.Aether;
+
 import de.invesdwin.context.test.ATest;
+import de.invesdwin.util.assertions.Assertions;
 
 @NotThreadSafe
 public class MavenPropertiesTest extends ATest {
 
     @Test
     public void testResolve() throws DependencyResolutionException {
+        final Aether aether = new Aether(MavenProperties.REMOTE_REPOSITORIES,
+                MavenProperties.LOCAL_REPOSITORY_DIRECTORY);
         final DefaultArtifact artifact = new DefaultArtifact("junit", "junit-dep", "", "jar", "4.10");
-        final List<Artifact> deps = MavenProperties.newDefaultAether().resolve(artifact, "runtime");
+        final List<Artifact> deps = aether.resolve(artifact, "runtime");
         for (final Artifact dep : deps) {
             log.info("%s", dep.getFile());
         }
+        Assertions.checkNotEmpty(deps);
     }
 
 }
