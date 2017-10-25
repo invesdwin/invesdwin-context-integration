a27f625969889943a434eaa5fc434665fd5c662a
subes <gsubes@gmail.com>
2017-10-25 17:41:42 +0200

    camelcase

--- invesdwin-context-integration-parent/invesdwin-context-integration-ws/src/main/java/de/invesdwin/context/integration/ws/registry/IRestRegistryService.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-ws/src/main/java/de/invesdwin/context/integration/ws/registry/IRestRegistryService.java
@@ -9,9 +9,9 @@ public interface IRestRegistryService {
     String ACCESS_URI = "accessUri";
     String ACCESS_URI_PARAM = "{" + ACCESS_URI + "}";
 
-    String REGISTER_SERVICE_BINDING = "registerservicebinding_" + SERVICE_NAME_PARAM + "_" + ACCESS_URI_PARAM;
-    String UNREGISTER_SERVICE_BINDING = "unregisterservicebinding_" + SERVICE_NAME_PARAM + "_" + ACCESS_URI_PARAM;
-    String QUERY_SERVICE_BINDINGS = "queryservicebindings_" + SERVICE_NAME_PARAM;
+    String REGISTER_SERVICE_BINDING = "registerServiceBinding_" + SERVICE_NAME_PARAM + "_" + ACCESS_URI_PARAM;
+    String UNREGISTER_SERVICE_BINDING = "unregisterServiceBinding_" + SERVICE_NAME_PARAM + "_" + ACCESS_URI_PARAM;
+    String QUERY_SERVICE_BINDINGS = "queryServiceBindings_" + SERVICE_NAME_PARAM;
     String INFO = "info";
     String AVAILABLE = "available";
     String CLIENTIP = "clientip";
