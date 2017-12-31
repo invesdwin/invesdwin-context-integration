ac44d58ba2bb81a32cdba0dcd67acfc92d67d777
subes <gsubes@gmail.com>
2017-12-31 23:43:15 +0100

    use threadlocal

--- invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/integration/jppf/RemoteFastJPPFSerialization.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/integration/jppf/RemoteFastJPPFSerialization.java
@@ -3,7 +3,6 @@ package de.invesdwin.integration.jppf;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.Immutable;
 
 import org.apache.commons.io.IOUtils;
@@ -16,19 +15,25 @@ import org.nustaq.serialization.FSTConfiguration;
 @Immutable
 public class RemoteFastJPPFSerialization implements JPPFSerialization {
 
-    @GuardedBy("this")
-    private final FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
+    private final ThreadLocal<FSTConfiguration> confThreadLocal = new ThreadLocal<FSTConfiguration>() {
+        @Override
+        protected FSTConfiguration initialValue() {
+            return FSTConfiguration.createDefaultConfiguration();
+        }
+    };
 
     public RemoteFastJPPFSerialization() {}
 
     @Override
     public synchronized void serialize(final Object o, final OutputStream os) throws Exception {
+        final FSTConfiguration conf = confThreadLocal.get();
         final byte[] bytes = conf.asByteArray(o);
         IOUtils.write(bytes, os);
     }
 
     @Override
     public synchronized Object deserialize(final InputStream is) throws Exception {
+        final FSTConfiguration conf = confThreadLocal.get();
         final ClassLoader previousClassLoader = conf.getClassLoader();
         try {
             final byte[] bytes = IOUtils.toByteArray(is);
