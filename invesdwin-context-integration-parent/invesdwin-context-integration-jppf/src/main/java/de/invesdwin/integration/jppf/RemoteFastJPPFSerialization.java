85d5d0344cd0b08350a729cc41b6796212368f53
subes <gsubes@gmail.com>
2017-12-31 19:02:51 +0100

    use context classloader

--- invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/integration/jppf/RemoteFastJPPFSerialization.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/integration/jppf/RemoteFastJPPFSerialization.java
@@ -7,6 +7,12 @@ import javax.annotation.concurrent.Immutable;
 
 import org.apache.commons.io.IOUtils;
 import org.jppf.serialization.JPPFSerialization;
+import org.nustaq.serialization.FSTConfiguration;
+import org.nustaq.serialization.FSTConfiguration.StreamCoderFactory;
+import org.nustaq.serialization.FSTDecoder;
+import org.nustaq.serialization.FSTEncoder;
+import org.nustaq.serialization.coders.FSTStreamDecoder;
+import org.nustaq.serialization.coders.FSTStreamEncoder;
 import org.nustaq.serialization.simpleapi.DefaultCoder;
 import org.nustaq.serialization.simpleapi.FSTCoder;
 
@@ -17,6 +23,44 @@ import org.nustaq.serialization.simpleapi.FSTCoder;
 public class RemoteFastJPPFSerialization implements JPPFSerialization {
 
     private final FSTCoder coder = new DefaultCoder(true);
+
+    {
+        final FSTConfiguration conf = coder.getConf();
+        conf.setStreamCoderFactory(new StreamCoderFactory() {
+
+            private final ThreadLocal<?> input = new ThreadLocal<Object>();
+            private final ThreadLocal<?> output = new ThreadLocal<Object>();
+
+            @Override
+            public FSTEncoder createStreamEncoder() {
+                return new FSTStreamEncoder(conf);
+            }
+
+            @Override
+            public FSTDecoder createStreamDecoder() {
+                return new FSTStreamDecoder(conf) {
+                    @Override
+                    public Class<?> classForName(final String name) throws ClassNotFoundException {
+                        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+                        if (cl == null) {
+                            return super.classForName(name);
+                        }
+                        return Class.forName(name, false, cl);
+                    }
+                };
+            }
+
+            @Override
+            public ThreadLocal<?> getInput() {
+                return input;
+            }
+
+            @Override
+            public ThreadLocal<?> getOutput() {
+                return output;
+            }
+        });
+    }
 
     @Override
     public void serialize(final Object o, final OutputStream os) throws Exception {
