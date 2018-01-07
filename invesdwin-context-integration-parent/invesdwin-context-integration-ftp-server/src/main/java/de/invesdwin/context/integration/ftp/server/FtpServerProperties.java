1c29d11a66aea0faaa772201c27fd829e5dd799d
subes <gsubes@gmail.com>
2018-01-07 18:19:45 +0100

    maxthreads removed

--- invesdwin-context-integration-parent/invesdwin-context-integration-ftp-server/src/main/java/de/invesdwin/context/integration/ftp/server/FtpServerProperties.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-ftp-server/src/main/java/de/invesdwin/context/integration/ftp/server/FtpServerProperties.java
@@ -19,21 +19,13 @@ public final class FtpServerProperties {
 
     public static final File BASE_DIRECTORY;
     public static final Integer PORT;
-    public static final int MAX_THREADS;
     public static final Duration PURGE_FILES_OLDER_THAN_DURATION;
 
-    private static final String KEY_MAX_THREADS = "MAX_THREADS";
     private static final String KEY_PURGE_FILES_OLDER_THAN_DURATION = "PURGE_FILES_OLDER_THAN_DURATION";
 
     static {
         final SystemProperties systemProperties = new SystemProperties(FtpServerProperties.class);
         PORT = systemProperties.getPort("PORT", true);
-        if (systemProperties.containsValue(KEY_MAX_THREADS)) {
-            MAX_THREADS = systemProperties.getInteger(KEY_MAX_THREADS);
-        } else {
-            MAX_THREADS = Runtime.getRuntime().availableProcessors();
-            systemProperties.setInteger(KEY_MAX_THREADS, MAX_THREADS);
-        }
         if (systemProperties.containsValue(KEY_PURGE_FILES_OLDER_THAN_DURATION)) {
             PURGE_FILES_OLDER_THAN_DURATION = systemProperties.getDuration(KEY_PURGE_FILES_OLDER_THAN_DURATION);
         } else {
