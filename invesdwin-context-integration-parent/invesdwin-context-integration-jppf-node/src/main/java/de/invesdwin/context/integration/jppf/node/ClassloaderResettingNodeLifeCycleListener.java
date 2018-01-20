4ad3e4dfd390e94fb1037096a50158a2abf1c278
subes <gsubes@gmail.com>
2018-01-20 13:32:57 +0100

    reset classloader for jobs

---
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf-node/src/main/java/de/invesdwin/context/integration/jppf/node/ClassloaderResettingNodeLifeCycleListener.java
@@ -0,0 +1,19 @@
+package de.invesdwin.context.integration.jppf.node;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.jppf.node.event.NodeLifeCycleEvent;
+import org.jppf.node.event.NodeLifeCycleListenerAdapter;
+
+@Immutable
+public class ClassloaderResettingNodeLifeCycleListener extends NodeLifeCycleListenerAdapter {
+
+    /**
+     * Use a fresh classloader for each job to prevent classcastexceptions from occuring
+     */
+    @Override
+    public void jobHeaderLoaded(final NodeLifeCycleEvent event) {
+        event.getNode().resetTaskClassLoader();
+    }
+
+}
