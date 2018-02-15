5a0deb1f23fa9047fd4b74720de4927a908e8b20
subes <gsubes@gmail.com>
2018-02-15 15:23:30 +0100

    synchronize

--- invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/context/integration/jppf/client/JPPFProcessingThreadsCounter.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/context/integration/jppf/client/JPPFProcessingThreadsCounter.java
@@ -357,8 +357,10 @@ public class JPPFProcessingThreadsCounter {
         boolean firstRun = !warmupFinished;
         do {
             if ((timeout != null && start.toDuration().isGreaterThan(timeout))) {
-                if (getDriversCount() == 0) {
-                    topologyManager.getJPPFClient().reset();
+                synchronized (this) {
+                    if (driverInfos.isEmpty()) {
+                        topologyManager.getJPPFClient().reset();
+                    }
                 }
                 throw new TimeoutException("timeout exceeded: " + timeout);
             }
