acd883e876171388a1aa415802e72f19bca846ce
subes <gsubes@gmail.com>
2018-02-25 21:26:12 +0100

    getClient

--- invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/context/integration/jppf/client/ConfiguredJPPFExecutorService.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf/src/main/java/de/invesdwin/context/integration/jppf/client/ConfiguredJPPFExecutorService.java
@@ -8,13 +8,19 @@ import org.jppf.client.concurrent.JPPFExecutorService;
 @ThreadSafe
 public class ConfiguredJPPFExecutorService extends JPPFExecutorService {
 
+    private final JPPFClient client;
+
     public ConfiguredJPPFExecutorService(final JPPFClient client) {
         super(client);
+        this.client = client;
         getConfiguration().getJobConfiguration().getClientSLA().setMaxChannels(1);
         //this prevents the same task from being executed more than once in parallel, it is better to relaunch the task in a new job instead
         getConfiguration().getJobConfiguration().getSLA().setMaxTaskResubmits(0);
         getConfiguration().getJobConfiguration().getSLA().setApplyMaxResubmitsUponNodeError(true);
-        getConfiguration().getJobConfiguration().getSLA().setMaxNodes(1);
+    }
+
+    public JPPFClient getClient() {
+        return client;
     }
 
 }
