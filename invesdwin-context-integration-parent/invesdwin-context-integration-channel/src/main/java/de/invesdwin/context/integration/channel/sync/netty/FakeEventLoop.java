f991d4a54b799b5a0fa96d37dfe63693ea0d59c9
subes <gsubes@gmail.com>
2021-09-07 01:24:35 +0200

    experiment

---
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/sync/netty/FakeEventLoop.java
@@ -0,0 +1,102 @@
+package de.invesdwin.context.integration.channel.sync.netty;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.DefaultChannelPromise;
+import io.netty.channel.EventLoop;
+import io.netty.channel.EventLoopGroup;
+import io.netty.util.concurrent.AbstractScheduledEventExecutor;
+import io.netty.util.concurrent.Future;
+
+@NotThreadSafe
+public class FakeEventLoop extends AbstractScheduledEventExecutor implements EventLoop {
+
+    @Override
+    public EventLoopGroup parent() {
+        return (EventLoopGroup) super.parent();
+    }
+
+    @Override
+    public EventLoop next() {
+        return (EventLoop) super.next();
+    }
+
+    @Override
+    public void execute(final Runnable command) {
+        command.run();
+    }
+
+    @Override
+    protected void cancelScheduledTasks() {
+        super.cancelScheduledTasks();
+    }
+
+    @Override
+    public Future<?> shutdownGracefully(final long quietPeriod, final long timeout, final TimeUnit unit) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Future<?> terminationFuture() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @Deprecated
+    public void shutdown() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isShuttingDown() {
+        return false;
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return false;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return false;
+    }
+
+    @Override
+    public boolean awaitTermination(final long timeout, final TimeUnit unit) {
+        return false;
+    }
+
+    @Override
+    public ChannelFuture register(final Channel channel) {
+        return register(new DefaultChannelPromise(channel, this));
+    }
+
+    @Override
+    public ChannelFuture register(final ChannelPromise promise) {
+        promise.channel().unsafe().register(this, promise);
+        return promise;
+    }
+
+    @Deprecated
+    @Override
+    public ChannelFuture register(final Channel channel, final ChannelPromise promise) {
+        channel.unsafe().register(this, promise);
+        return promise;
+    }
+
+    @Override
+    public boolean inEventLoop() {
+        return true;
+    }
+
+    @Override
+    public boolean inEventLoop(final Thread thread) {
+        return true;
+    }
+}
--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/sync/netty/tcp/NettySocketSynchronousWriter.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/sync/netty/tcp/NettySocketSynchronousWriter.java
@@ -1,17 +1,24 @@
 package de.invesdwin.context.integration.channel.sync.netty.tcp;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
 import de.invesdwin.context.integration.channel.sync.ISynchronousWriter;
+import de.invesdwin.context.integration.channel.sync.netty.FakeEventLoop;
 import de.invesdwin.util.streams.buffer.ClosedByteBuffer;
 import de.invesdwin.util.streams.buffer.IByteBufferWriter;
 import de.invesdwin.util.streams.buffer.delegate.NettyDelegateByteBuffer;
 import de.invesdwin.util.streams.buffer.delegate.slice.SlicedFromDelegateByteBuffer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPromise;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 @NotThreadSafe
 public class NettySocketSynchronousWriter implements ISynchronousWriter<IByteBufferWriter> {
@@ -28,7 +35,8 @@ public class NettySocketSynchronousWriter implements ISynchronousWriter<IByteBuf
     @Override
     public void open() throws IOException {
         channel.open(null);
-        //        channel.getSocketChannel().deregister();
+        channel.getSocketChannel().deregister();
+        new FakeEventLoop().register(channel.getSocketChannel());
         //netty uses direct buffer per default
         this.buf = Unpooled.directBuffer(channel.getSocketSize());
         this.buf.retain();
@@ -39,11 +47,7 @@ public class NettySocketSynchronousWriter implements ISynchronousWriter<IByteBuf
     @Override
     public void close() {
         if (buffer != null) {
-            try {
-                writeFuture(ClosedByteBuffer.INSTANCE).sync();
-            } catch (final Throwable t) {
-                //ignore
-            }
+            writeFuture(ClosedByteBuffer.INSTANCE);
             buf.release();
             buf = null;
             buffer = null;
@@ -60,12 +64,172 @@ public class NettySocketSynchronousWriter implements ISynchronousWriter<IByteBuf
         writeFuture(message);
     }
 
-    private ChannelFuture writeFuture(final IByteBufferWriter message) {
+    private void writeFuture(final IByteBufferWriter message) {
         final int size = message.write(messageBuffer);
         buffer.putInt(NettySocketChannel.SIZE_INDEX, size);
         buf.setIndex(0, NettySocketChannel.MESSAGE_INDEX + size);
         buf.retain(); //keep retain count up
-        return channel.getSocketChannel().writeAndFlush(buf);
+        channel.getSocketChannel().unsafe().write(buf, new ChannelPromise() {
+
+            @Override
+            public boolean trySuccess(final Void result) {
+                return false;
+            }
+
+            @Override
+            public boolean tryFailure(final Throwable cause) {
+                return false;
+            }
+
+            @Override
+            public boolean setUncancellable() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public Void get(final long timeout, final TimeUnit unit)
+                    throws InterruptedException, ExecutionException, TimeoutException {
+                return null;
+            }
+
+            @Override
+            public Void get() throws InterruptedException, ExecutionException {
+                return null;
+            }
+
+            @Override
+            public boolean isSuccess() {
+                return false;
+            }
+
+            @Override
+            public boolean isCancellable() {
+                return false;
+            }
+
+            @Override
+            public Void getNow() {
+                return null;
+            }
+
+            @Override
+            public Throwable cause() {
+                return null;
+            }
+
+            @Override
+            public boolean cancel(final boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean awaitUninterruptibly(final long timeout, final TimeUnit unit) {
+                return false;
+            }
+
+            @Override
+            public boolean awaitUninterruptibly(final long timeoutMillis) {
+                return false;
+            }
+
+            @Override
+            public boolean await(final long timeout, final TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public boolean await(final long timeoutMillis) throws InterruptedException {
+                return false;
+            }
+
+            @Override
+            public boolean isVoid() {
+                return false;
+            }
+
+            @Override
+            public ChannelPromise unvoid() {
+                return null;
+            }
+
+            @Override
+            public boolean trySuccess() {
+                return false;
+            }
+
+            @Override
+            public ChannelPromise syncUninterruptibly() {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise sync() throws InterruptedException {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise setSuccess(final Void result) {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise setSuccess() {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise setFailure(final Throwable cause) {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise removeListeners(
+                    final GenericFutureListener<? extends Future<? super Void>>... listeners) {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise removeListener(final GenericFutureListener<? extends Future<? super Void>> listener) {
+                return null;
+            }
+
+            @Override
+            public Channel channel() {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise awaitUninterruptibly() {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise await() throws InterruptedException {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise addListeners(
+                    final GenericFutureListener<? extends Future<? super Void>>... listeners) {
+                return null;
+            }
+
+            @Override
+            public ChannelPromise addListener(final GenericFutureListener<? extends Future<? super Void>> listener) {
+                return null;
+            }
+        });
+        channel.getSocketChannel().unsafe().flush();
     }
 
 }
