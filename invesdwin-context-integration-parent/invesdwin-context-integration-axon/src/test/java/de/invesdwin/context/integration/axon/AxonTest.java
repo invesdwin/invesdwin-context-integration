51ef8b29a7ebd700fdf1f9055d2ad8023c6388a9
Edwin Stang <gsubes@gmail.com>
2022-03-03 09:26:21 +0100

    axontest

---
+++ invesdwin-context-integration-parent/invesdwin-context-integration-axon/src/test/java/de/invesdwin/context/integration/axon/AxonTest.java
@@ -0,0 +1,10 @@
+package de.invesdwin.context.integration.axon;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import de.invesdwin.context.test.ATest;
+
+@NotThreadSafe
+public class AxonTest extends ATest {
+
+}
