fc24015412a6abb2fbf43b7e3b1f5e4c2824af23
Edwin Stang <gsubes@gmail.com>
2022-08-20 18:17:41 +0200

    disable domain socket channel tests

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/sync/socket/domain/unsafe/BidiNativeUnixDomainSocketChannelTest.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/sync/socket/domain/unsafe/BidiNativeUnixDomainSocketChannelTest.java
@@ -1,45 +1,39 @@
 package de.invesdwin.context.integration.channel.sync.socket.domain.unsafe;
 
-import java.io.IOException;
-import java.net.SocketAddress;
-import java.net.StandardProtocolFamily;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-
 import javax.annotation.concurrent.NotThreadSafe;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
-import de.invesdwin.context.integration.channel.sync.socket.tcp.SocketSynchronousChannel;
 import de.invesdwin.context.integration.channel.sync.socket.tcp.unsafe.BidiNativeSocketChannelTest;
 
+@Disabled
 @NotThreadSafe
 public class BidiNativeUnixDomainSocketChannelTest extends BidiNativeSocketChannelTest {
 
     //https://nipafx.dev/java-unix-domain-sockets/ (requires java 16, thus commented out)
-    @Override
-    @Test
-    public void testBidiNioSocketPerformance() throws InterruptedException {
-        final SocketAddress address = java.net.UnixDomainSocketAddress
-                .of(newFile("testBidiNioSocketPerformance", true, FileChannelType.UNIX_SOCKET).getAbsolutePath());
-        runNioSocketPerformanceTest(address);
-    }
-
-    @Override
-    protected SocketSynchronousChannel newSocketSynchronousChannel(final SocketAddress socketAddress,
-            final boolean server, final int estimatedMaxMessageSize) {
-        final StandardProtocolFamily protocolFamily = StandardProtocolFamily.UNIX;
-        return new SocketSynchronousChannel(socketAddress, server, estimatedMaxMessageSize) {
-            @Override
-            protected ServerSocketChannel newServerSocketChannel() throws IOException {
-                return ServerSocketChannel.open(protocolFamily);
-            }
-
-            @Override
-            protected SocketChannel newSocketChannel() throws IOException {
-                return SocketChannel.open(protocolFamily);
-            }
-        };
-    }
+    //    @Override
+    //    @Test
+    //    public void testBidiNioSocketPerformance() throws InterruptedException {
+    //        final SocketAddress address = java.net.UnixDomainSocketAddress
+    //                .of(newFile("testBidiNioSocketPerformance", true, FileChannelType.UNIX_SOCKET).getAbsolutePath());
+    //        runNioSocketPerformanceTest(address);
+    //    }
+    //
+    //    @Override
+    //    protected SocketSynchronousChannel newSocketSynchronousChannel(final SocketAddress socketAddress,
+    //            final boolean server, final int estimatedMaxMessageSize) {
+    //        final StandardProtocolFamily protocolFamily = StandardProtocolFamily.UNIX;
+    //        return new SocketSynchronousChannel(socketAddress, server, estimatedMaxMessageSize) {
+    //            @Override
+    //            protected ServerSocketChannel newServerSocketChannel() throws IOException {
+    //                return ServerSocketChannel.open(protocolFamily);
+    //            }
+    //
+    //            @Override
+    //            protected SocketChannel newSocketChannel() throws IOException {
+    //                return SocketChannel.open(protocolFamily);
+    //            }
+    //        };
+    //    }
 
 }
