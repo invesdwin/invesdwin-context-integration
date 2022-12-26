9d575ccd3b35f270d05fd5f14d75285498ad1f1f
Edwin Stang <gsubes@gmail.com>
2022-12-26 17:13:01 +0100

    999

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/sync/netty/udt/NettyUdtSynchronousChannelTest.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/sync/netty/udt/NettyUdtSynchronousChannelTest.java
@@ -4,6 +4,7 @@ import java.net.InetSocketAddress;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import de.invesdwin.context.integration.channel.AChannelTest;
@@ -16,6 +17,7 @@ import de.invesdwin.util.concurrent.Executors;
 import de.invesdwin.util.concurrent.WrappedExecutorService;
 import de.invesdwin.util.streams.buffer.bytes.IByteBufferProvider;
 
+@Disabled
 @NotThreadSafe
 public class NettyUdtSynchronousChannelTest extends AChannelTest {
 
