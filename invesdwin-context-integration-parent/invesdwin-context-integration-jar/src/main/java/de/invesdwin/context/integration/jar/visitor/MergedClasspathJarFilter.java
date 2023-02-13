1ff0138ab551bf2e44b139c87182c6c564bd31a7
Edwin Stang <gsubes@gmail.com>
2023-02-13 22:10:59 +0100

    MPI blacklist

--- invesdwin-context-integration-parent/invesdwin-context-integration-jar/src/main/java/de/invesdwin/context/integration/jar/visitor/MergedClasspathJarFilter.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jar/src/main/java/de/invesdwin/context/integration/jar/visitor/MergedClasspathJarFilter.java
@@ -28,6 +28,17 @@ public enum MergedClasspathJarFilter implements IMergedClasspathJarFilter {
         public String[] getWhitelist() {
             return HADOOP_WHITELIST;
         }
+    },
+    MPI {
+        @Override
+        public String[] getBlacklist() {
+            return MPI_BLACKLIST;
+        }
+
+        @Override
+        public String[] getWhitelist() {
+            return Strings.EMPTY_ARRAY;
+        }
     };
 
     //<filters>
@@ -43,17 +54,20 @@ public enum MergedClasspathJarFilter implements IMergedClasspathJarFilter {
     private static final String[] DEFAULT_BLACKLIST = { "META-INF/.*\\.SF", "META-INF/.*\\.DSA", "META-INF/.*\\.RSA",
             "META-INF/MANIFEST.MF", "META-INF/INDEX.LIST" };
 
-    private static final String[] HADOOP_BLACKLIST = Arrays.concat(DEFAULT_BLACKLIST, new String[] {
-            // exclude packages that hadoop has itself in classpath to prevent clashes
-            "org/apache/hadoop/.*", "javax/inject/.*", "org/codehaus/jackson/.*", "org/mockito/.*",
-            "org/apache/commons/collections/.*", "com/google/common/.*", "org/apache/commons/compress/.*",
-            "com/google/protobuf/.*", "org/apache/commons/math/.*", "org/aopalliance/.*", "org/apache/commons/lang/.*",
-            "org/apache/avro/.*", "com/thoughtworks/paranamer/.*", "org/junit/.*", "org/objenesis/.*",
-            "org/hamcrest/.*", "org/apache/commons/beanutils/.*", "org/xerial/snappy/.*", "javax/annotation/.*",
-            "org/apache/commons/configuration/.*", "org/slf4j/.*", "org/apache/log4j/.*", "org/apache/commons/io/.*",
-            "org/apache/commons/codec/.*", "junit/.*", "org/apache/commons/logging/.*", "javax/el/.*",
-            "org/apache/commons/cli/.*", "org/codehaus/jettison/.*" });
-
+    // exclude packages that hadoop has itself in classpath to prevent clashes
+    private static final String[] HADOOP_BLACKLIST = Arrays.concat(DEFAULT_BLACKLIST,
+            new String[] { "org/apache/hadoop/.*", "javax/inject/.*", "org/codehaus/jackson/.*", "org/mockito/.*",
+                    "org/apache/commons/collections/.*", "com/google/common/.*", "org/apache/commons/compress/.*",
+                    "com/google/protobuf/.*", "org/apache/commons/math/.*", "org/aopalliance/.*",
+                    "org/apache/commons/lang/.*", "org/apache/avro/.*", "com/thoughtworks/paranamer/.*", "org/junit/.*",
+                    "org/objenesis/.*", "org/hamcrest/.*", "org/apache/commons/beanutils/.*", "org/xerial/snappy/.*",
+                    "javax/annotation/.*", "org/apache/commons/configuration/.*", "org/slf4j/.*", "org/apache/log4j/.*",
+                    "org/apache/commons/io/.*", "org/apache/commons/codec/.*", "junit/.*",
+                    "org/apache/commons/logging/.*", "javax/el/.*", "org/apache/commons/cli/.*",
+                    "org/codehaus/jettison/.*" });
     private static final String[] HADOOP_WHITELIST = { "org/slf4j/ext/.*" };
+
+    private static final String[] MPI_BLACKLIST = Arrays.concat(DEFAULT_BLACKLIST,
+            new String[] { "mpi/.*", "mpjbuf/.*", "mpjdev/.*", "xdev/.*" });
 
 }
