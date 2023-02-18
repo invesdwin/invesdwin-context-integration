10cb499afe0bbc0062b40f58756f78856f2cdf35
Edwin Stang <gsubes@gmail.com>
2023-02-18 20:01:12 +0100

    java 8 warning

--- invesdwin-context-integration-parent/invesdwin-context-integration-mpi-test/mpj/mpjexpress_compile.sh
+++ invesdwin-context-integration-parent/invesdwin-context-integration-mpi-test/mpj/mpjexpress_compile.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
+#https://stackoverflow.com/questions/7334754/correct-way-to-check-java-version-from-bash-script
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" = "1.8" ]]; then
+        JAVA8="true"
+    else
+        echo "WARNING: skipping projects in build that require java 1.8 to build without errors"
+        JAVA8="false"
+    fi
+fi
+
+if [[ "$JAVA8" = "false" ]]; then
+    echo "requires Java 8 to compile against Hadoop"
+    exit 1
+fi
+
 BASEDIR=`dirname $0`
 cd $BASEDIR
 
