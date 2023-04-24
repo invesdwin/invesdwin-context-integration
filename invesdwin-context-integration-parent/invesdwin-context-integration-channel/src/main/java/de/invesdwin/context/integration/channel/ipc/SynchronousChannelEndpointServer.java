4032e31784bc893623b96d624b87de3b0f8c7641
Edwin Stang <gsubes@gmail.com>
2023-04-24 23:43:23 +0200

    thread safe service registration

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/ipc/SynchronousChannelEndpointServer.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/ipc/SynchronousChannelEndpointServer.java
@@ -1,6 +1,7 @@
 package de.invesdwin.context.integration.channel.ipc;
 
-import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
 
 import de.invesdwin.context.integration.channel.sync.ISynchronousReader;
 import de.invesdwin.util.marshallers.serde.ISerde;
@@ -9,13 +10,15 @@ import de.invesdwin.util.time.duration.Duration;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
-@NotThreadSafe
+@ThreadSafe
 public class SynchronousChannelEndpointServer {
 
     private final ISynchronousReader<ISynchronousChannelEndpoint<IByteBufferProvider, IByteBufferProvider>> serverAcceptor;
     private final ISerde<Object> genericSerde;
     private final Duration requestTimeout;
-    private final Int2ObjectMap<SynchronousChannelEndpointService> interfaceTypeId_service = new Int2ObjectOpenHashMap<>();
+    @GuardedBy("this")
+    private final Int2ObjectMap<SynchronousChannelEndpointService> interfaceTypeId_service_sync = new Int2ObjectOpenHashMap<>();
+    private volatile Int2ObjectMap<SynchronousChannelEndpointService> interfaceTypeId_service_copy = new Int2ObjectOpenHashMap<>();
 
     public SynchronousChannelEndpointServer(
             final ISynchronousReader<ISynchronousChannelEndpoint<IByteBufferProvider, IByteBufferProvider>> serverAcceptor,
@@ -25,20 +28,22 @@ public class SynchronousChannelEndpointServer {
         this.requestTimeout = requestTimeout;
     }
 
-    public <T> boolean register(final Class<? super T> interfaceType, final T implementation) {
+    public synchronized <T> void register(final Class<? super T> interfaceType, final T implementation) {
         final SynchronousChannelEndpointService service = SynchronousChannelEndpointService.newInstance(interfaceType,
                 implementation, genericSerde);
-        final SynchronousChannelEndpointService existing = interfaceTypeId_service
+        final SynchronousChannelEndpointService existing = interfaceTypeId_service_sync
                 .putIfAbsent(service.getInterfaceTypeId(), service);
         if (existing != null) {
             throw new IllegalStateException("Already registered [" + service + "] as [" + existing + "]");
         }
-        return true;
+
+        //create a new copy of the map so that server thread does not require synchronization
+        this.interfaceTypeId_service_copy = new Int2ObjectOpenHashMap<>(interfaceTypeId_service_sync);
     }
 
-    public <T> boolean unregister(final Class<? super T> interfaceType) {
+    public synchronized <T> boolean unregister(final Class<? super T> interfaceType) {
         final int interfaceTypeId = SynchronousChannelEndpointService.newInterfaceTypeId(interfaceType);
-        final SynchronousChannelEndpointService removed = interfaceTypeId_service.remove(interfaceTypeId);
+        final SynchronousChannelEndpointService removed = interfaceTypeId_service_sync.remove(interfaceTypeId);
         return removed != null;
     }
 
