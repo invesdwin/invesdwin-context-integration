27b39ebe8e7075c91ed64a426306b7c288c1c3a1
Edwin Stang <gsubes@gmail.com>
2023-05-01 21:42:46 +0200

    move todos

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/server/session/SynchronousEndpointServerSession.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/server/session/SynchronousEndpointServerSession.java
@@ -87,6 +87,19 @@ public class SynchronousEndpointServerSession implements Closeable {
     }
 
     public boolean handle() {
+        //            try {
+        //                while (true) {
+        //                    //TODO look for requests in clients, dispatch request handling and response sending to worker (handle heartbeat as well), return client for request monitoring after completion
+        //                    //reject executions if too many pending count for worker pool
+        //                    //check on start of worker task if timeout is already exceeded and abort directly (might have been in queue for too long)
+        //                    //maybe return exceptions to clients (similar to RmiExceptions that contain the stacktrace as message, full stacktrace in testing only?)
+        //                    //handle writeFinished in io thread (maybe the better idea?)
+        //                    return;
+        //                }
+        //            } finally {
+        //                cancelRemainingWorkerFutures();
+        //            }
+        //return true if work was done
         return false;
     }
 
