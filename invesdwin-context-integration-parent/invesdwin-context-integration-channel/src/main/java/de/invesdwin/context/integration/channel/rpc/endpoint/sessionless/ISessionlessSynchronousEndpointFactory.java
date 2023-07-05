dfb1f55684618bc76035f04f7fdade80c0162f37
Edwin Stang <gsubes@gmail.com>
2023-07-05 20:54:49 +0200

    extend endpoint factory

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/endpoint/sessionless/ISessionlessSynchronousEndpointFactory.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/endpoint/sessionless/ISessionlessSynchronousEndpointFactory.java
@@ -1,7 +1,10 @@
 package de.invesdwin.context.integration.channel.rpc.endpoint.sessionless;
 
-public interface ISessionlessSynchronousEndpointFactory<R, W, O> {
+import de.invesdwin.context.integration.channel.rpc.endpoint.ISynchronousEndpointFactory;
 
+public interface ISessionlessSynchronousEndpointFactory<R, W, O> extends ISynchronousEndpointFactory<R, W> {
+
+    @Override
     ISessionlessSynchronousEndpoint<R, W, O> newEndpoint();
 
 }
