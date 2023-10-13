137f6473a9aad4020fef74d484469483f41bff02
Edwin Stang <gsubes@gmail.com>
2023-10-14 01:06:06 +0200

    no open

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/rmi/RmiSynchronousEndpointClientSessionFactory.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/rmi/RmiSynchronousEndpointClientSessionFactory.java
@@ -1,24 +1,17 @@
 package de.invesdwin.context.integration.channel.rpc.rmi;
 
-import java.io.IOException;
-
 import javax.annotation.concurrent.Immutable;
 
 import de.invesdwin.context.integration.channel.rpc.endpoint.session.ISynchronousEndpointSession;
 import de.invesdwin.context.integration.channel.rpc.endpoint.session.ISynchronousEndpointSessionFactory;
-import de.invesdwin.context.integration.channel.sync.ISynchronousChannel;
 
 @Immutable
-public class RmiSynchronousEndpointClientSessionFactory
-        implements ISynchronousEndpointSessionFactory, ISynchronousChannel {
+public class RmiSynchronousEndpointClientSessionFactory implements ISynchronousEndpointSessionFactory {
 
     @Override
     public ISynchronousEndpointSession newSession() {
         return null;
     }
-
-    @Override
-    public void open() throws IOException {}
 
     @Override
     public void close() {}
