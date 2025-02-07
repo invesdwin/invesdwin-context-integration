cc046ebe71b0cea23b4caf632ec0cf1457dae97d
Edwin Stang <gsubes@gmail.com>
2025-02-07 16:52:48 +0100

    https://github.com/invesdwin/invesdwin-context-integration/issues/14 remove throughput handler since request/response is irrelevant for this

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/AThroughputChannelTest.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/AThroughputChannelTest.java
@@ -4,7 +4,6 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.NoSuchElementException;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 
@@ -12,9 +11,6 @@ import javax.annotation.concurrent.NotThreadSafe;
 
 import org.zeroturnaround.exec.stream.slf4j.Slf4jStream;
 
-import de.invesdwin.context.integration.channel.async.AsynchronousHandlerFactorySupport;
-import de.invesdwin.context.integration.channel.async.IAsynchronousHandler;
-import de.invesdwin.context.integration.channel.async.IAsynchronousHandlerContext;
 import de.invesdwin.context.integration.channel.sync.DisabledChannelFactory;
 import de.invesdwin.context.integration.channel.sync.ISynchronousChannelFactory;
 import de.invesdwin.context.integration.channel.sync.ISynchronousReader;
@@ -27,11 +23,9 @@ import de.invesdwin.context.integration.channel.sync.spinwait.SynchronousReaderS
 import de.invesdwin.context.integration.channel.sync.spinwait.SynchronousWriterSpinWait;
 import de.invesdwin.context.log.Log;
 import de.invesdwin.util.assertions.Assertions;
-import de.invesdwin.util.collections.iterable.ICloseableIterator;
 import de.invesdwin.util.concurrent.Executors;
 import de.invesdwin.util.concurrent.WrappedExecutorService;
 import de.invesdwin.util.concurrent.reference.IReference;
-import de.invesdwin.util.error.FastEOFException;
 import de.invesdwin.util.lang.Files;
 import de.invesdwin.util.streams.buffer.bytes.IByteBufferProvider;
 import de.invesdwin.util.time.Instant;
@@ -209,162 +203,6 @@ public abstract class AThroughputChannelTest extends AChannelTest {
                 channelWriter.close();
             } catch (final Exception e) {
                 throw new RuntimeException(e);
-            }
-        }
-
-    }
-
-    public static class ThroughputReaderHandlerFactory extends AsynchronousHandlerFactorySupport<FDate, FDate> {
-
-        @Override
-        public IAsynchronousHandler<FDate, FDate> newHandler() {
-            return new ThroughputReaderHandler();
-        }
-
-    }
-
-    public static class ThroughputReaderHandler implements IAsynchronousHandler<FDate, FDate> {
-
-        private final OutputStream log;
-        private Instant readsStart;
-        private int count;
-        private FDate prevValue;
-
-        public ThroughputReaderHandler() {
-            this(new Log(ThroughputReaderHandler.class));
-        }
-
-        public ThroughputReaderHandler(final Log log) {
-            this(Slf4jStream.of(log).asInfo());
-        }
-
-        public ThroughputReaderHandler(final OutputStream log) {
-            this.log = log;
-        }
-
-        @Override
-        public FDate open(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            readsStart = new Instant();
-            prevValue = null;
-            count = 0;
-            return REQUEST_MESSAGE;
-        }
-
-        @Override
-        public FDate idle(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            throw FastEOFException.getInstance("should not become idle");
-        }
-
-        @Override
-        public FDate handle(final IAsynchronousHandlerContext<FDate> context, final FDate readMessage)
-                throws IOException {
-            if (DEBUG) {
-                log.write("receiver request out\n".getBytes());
-            }
-            if (DEBUG) {
-                log.write(("receiver channel in [" + readMessage + "]\n").getBytes());
-            }
-            Assertions.checkNotNull(readMessage);
-            if (prevValue != null && !prevValue.isBefore(readMessage)) {
-                Assertions.assertThat(prevValue).isBefore(readMessage);
-            }
-            prevValue = readMessage;
-            count++;
-            return REQUEST_MESSAGE;
-        }
-
-        @Override
-        public void outputFinished(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            //noop
-        }
-
-        @Override
-        public void close() throws IOException {
-            Assertions.checkEquals(count, VALUES);
-            if (DEBUG) {
-                log.write("receiver close handler\n".getBytes());
-            }
-            printProgress(log, "ReadsFinished", readsStart, VALUES, VALUES);
-        }
-
-    }
-
-    public static class ThroughputWriterHandlerFactory extends AsynchronousHandlerFactorySupport<FDate, FDate> {
-
-        @Override
-        public IAsynchronousHandler<FDate, FDate> newHandler() {
-            return new ThroughputWriterHandler();
-        }
-
-    }
-
-    public static class ThroughputWriterHandler implements IAsynchronousHandler<FDate, FDate> {
-
-        private final OutputStream log;
-        private Instant writesStart;
-        private int i;
-        private ICloseableIterator<FDate> values;
-
-        public ThroughputWriterHandler() {
-            this(new Log(ThroughputWriterHandler.class));
-        }
-
-        public ThroughputWriterHandler(final Log log) {
-            this(Slf4jStream.of(log).asInfo());
-        }
-
-        public ThroughputWriterHandler(final OutputStream log) {
-            this.log = log;
-        }
-
-        @Override
-        public FDate open(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            writesStart = new Instant();
-            i = 0;
-            this.values = newValues().iterator();
-            if (DEBUG) {
-                log.write("sender open handler\n".getBytes());
-            }
-            return null;
-        }
-
-        @Override
-        public FDate idle(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            throw FastEOFException.getInstance("should not become idle");
-        }
-
-        @Override
-        public FDate handle(final IAsynchronousHandlerContext<FDate> context, final FDate readMessage)
-                throws IOException {
-            if (DEBUG) {
-                log.write("sender request in\n".getBytes());
-            }
-            Assertions.checkEquals(readMessage, REQUEST_MESSAGE);
-            try {
-                final FDate date = values.next();
-                if (DEBUG) {
-                    log.write(("sender channel out [" + date + "]\n").getBytes());
-                }
-                i++;
-                if (i % FLUSH_INTERVAL == 0) {
-                    printProgress(log, "Writes", writesStart, i, VALUES);
-                }
-                return date;
-            } catch (final NoSuchElementException e) {
-                throw FastEOFException.getInstance(e);
-            }
-        }
-
-        @Override
-        public void outputFinished(final IAsynchronousHandlerContext<FDate> context) throws IOException {
-            //noop
-        }
-
-        @Override
-        public void close() throws IOException {
-            printProgress(log, "WritesFinished", writesStart, VALUES, VALUES);
-            if (DEBUG) {
-                log.write("sender close handler\n".getBytes());
             }
         }
 
