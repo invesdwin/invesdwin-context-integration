1b89b4d7fa3f10a63c90e106fa375f12489eb9ee
Edwin Stang <gsubes@gmail.com>
2025-02-22 20:25:39 +0100

    separate by factoryId

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/base/endpoint/session/DefaultSynchronousEndpointSessionFactory.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/main/java/de/invesdwin/context/integration/channel/rpc/base/endpoint/session/DefaultSynchronousEndpointSessionFactory.java
@@ -12,17 +12,20 @@ import de.invesdwin.util.streams.buffer.bytes.IByteBufferProvider;
 @ThreadSafe
 public class DefaultSynchronousEndpointSessionFactory implements ISynchronousEndpointSessionFactory {
 
+    private static final AtomicLong NEXT_FACTORY_ID = new AtomicLong();
     private final ISynchronousEndpointFactory<IByteBufferProvider, IByteBufferProvider> endpointFactory;
-    private final AtomicLong nextId = new AtomicLong(0);
+    private final AtomicLong nextId = new AtomicLong();
+    private final long factoryId;
 
     public DefaultSynchronousEndpointSessionFactory(
             final ISynchronousEndpointFactory<IByteBufferProvider, IByteBufferProvider> endpointFactory) {
         this.endpointFactory = endpointFactory;
+        this.factoryId = NEXT_FACTORY_ID.incrementAndGet();
     }
 
     @Override
     public ISynchronousEndpointSession newSession() {
-        final String sessionId = Processes.getProcessId() + "_" + nextId.incrementAndGet();
+        final String sessionId = Processes.getProcessId() + "_" + factoryId + "_" + nextId.incrementAndGet();
         final ISynchronousEndpoint<IByteBufferProvider, IByteBufferProvider> endpoint = endpointFactory.newEndpoint();
         return new DefaultSynchronousEndpointSession(sessionId, endpoint, null);
     }
