e47f6bdb177ef3a3e8de27309de657cfd13af25b
angelo <angelo@angelo-B560M-D3H>
2025-03-17 15:28:15 +0000

    https://github.com/invesdwin/invesdwin-context-integration/issues/14 box and whiskers plot

--- invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/report/generate/HtmlBoxAndWhiskersLatencyReportTest.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-channel/src/test/java/de/invesdwin/context/integration/channel/report/generate/HtmlBoxAndWhiskersLatencyReportTest.java
@@ -15,20 +15,67 @@ import de.invesdwin.context.ContextProperties;
 import de.invesdwin.context.integration.html.distribution.DistributionMeasure;
 import de.invesdwin.context.integration.html.distribution.box.HtmlDistributionBoxAndWhiskerReport;
 import de.invesdwin.context.test.ATest;
+import de.invesdwin.util.collections.Collections;
+import de.invesdwin.util.collections.list.Lists;
+import de.invesdwin.util.lang.Files;
+import de.invesdwin.util.math.decimal.Decimal;
+import de.invesdwin.util.math.decimal.scaled.Percent;
+import de.invesdwin.util.math.decimal.scaled.PercentScale;
+import de.invesdwin.util.time.date.FTimeUnit;
 
 @NotThreadSafe
 public class HtmlBoxAndWhiskersLatencyReportTest extends ATest {
 
+    private static final String CSV_SEPARATOR = "\t";
+
     @Test
     public void generate() throws IOException {
-        final File folder = new File(ContextProperties.getLogDirectory(),
+        final FTimeUnit reportTimeUnit = FTimeUnit.MILLISECONDS;
+        final FTimeUnit measureTimeUnit = FTimeUnit.NANOSECONDS;
+        final int decimalPlaces = Decimal.valueOf(measureTimeUnit.convert(1, reportTimeUnit)).getWholeNumberDigits();
+
+        final File baseFolder = new File(ContextProperties.getLogDirectory(),
                 HtmlBoxAndWhiskersLatencyReportTest.class.getSimpleName());
-        final File file = new File(folder, "BoxAndWhiskers.html");
+
+        final File file = new File(baseFolder, "BoxAndWhiskers.html");
+
+        final File samplesFolder = new File(baseFolder, "samples");
+
         final Map<String, List<DistributionMeasure>> measure_samples = new LinkedHashMap<>();
         final List<DistributionMeasure> samples = new ArrayList<>();
-        //TODO angelo: read in a sample for each file that should be added to the box and whiskers plot based
+
+        final File[] fileObjects = samplesFolder.listFiles();
+
+        for (final File csvFile : fileObjects) {
+            final List<String> lines = Files.readAllLines(csvFile.toPath());
+
+            final List<Decimal> measures = new ArrayList<>();
+            for (int i = 1; i < lines.size(); i++) {
+                final String line = lines.get(i);
+                final String[] tokens = line.split(CSV_SEPARATOR);
+                try {
+                    final int index = Integer.parseInt(tokens[0]);
+                    if (index < 0) {
+                        continue;
+                    }
+                    final long latencyValueRaw = Long.parseLong(tokens[1]);
+                    final double latencyValue = reportTimeUnit.asFractional()
+                            .convert(latencyValueRaw, measureTimeUnit.asFractional());
+                    measures.add(Decimal.valueOf(latencyValue).round(decimalPlaces));
+                } catch (final NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            log.info(csvFile.getName() + ": " + measures.size());
+            Collections.sort(measures);
+            final int removeLength = (int) (measures.size() * new Percent(5, PercentScale.PERCENT).getRate());
+            Lists.removeRange(measures, measures.size() - removeLength, measures.size());
+            final String sampleName = Files.removeExtension(csvFile.getName());
+            final DistributionMeasure measure = new DistributionMeasure(sampleName, "ms", measures, false, 2);
+            samples.add(measure);
+        }
         measure_samples.put("Latency", samples);
         new HtmlDistributionBoxAndWhiskerReport().writeReport(file, measure_samples);
     }
-
 }
