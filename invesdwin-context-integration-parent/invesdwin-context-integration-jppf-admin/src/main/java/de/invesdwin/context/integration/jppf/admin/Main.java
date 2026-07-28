f56532f73404df6ad57502b85f70b19d45c2d435
Edwin Stang <gsubes@gmail.com>
2026-07-28 13:00:24 +0200

    scale configurer

--- invesdwin-context-integration-parent/invesdwin-context-integration-jppf-admin/src/main/java/de/invesdwin/context/integration/jppf/admin/Main.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-jppf-admin/src/main/java/de/invesdwin/context/integration/jppf/admin/Main.java
@@ -10,6 +10,7 @@ import de.invesdwin.context.beans.init.MergedContext;
 import de.invesdwin.context.integration.jppf.JPPFClientProperties;
 import de.invesdwin.context.integration.jppf.client.ConfiguredClientDriverDiscovery;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.swing.SunJava2dUIScaleConfigurer;
 
 @Immutable
 public final class Main {
@@ -17,6 +18,7 @@ public final class Main {
     private Main() {}
 
     public static void main(final String[] args) {
+        SunJava2dUIScaleConfigurer.configure();
         final JPPFSplash splash = new JPPFSplash("Connecting ...");
         splash.start();
         MergedContext.autowire(null);
