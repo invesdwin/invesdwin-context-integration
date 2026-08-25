7f28f0571f47dcfd8d700f6d9fdd6e0f8b1d4c9b
Edwin Stang <gsubes@gmail.com>
2026-08-25 17:01:39 +0200

    trigger periodic event every 10 seconds

--- invesdwin-context-integration-parent/invesdwin-context-integration-webdav-server/src/main/java/de/invesdwin/context/integration/webdav/server/internal/ConfiguredWebdavServlet.java
+++ invesdwin-context-integration-parent/invesdwin-context-integration-webdav-server/src/main/java/de/invesdwin/context/integration/webdav/server/internal/ConfiguredWebdavServlet.java
@@ -1,14 +1,20 @@
 package de.invesdwin.context.integration.webdav.server.internal;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.concurrent.Immutable;
 
 import org.apache.catalina.servlets.ExtendedWebdavServlet;
 import org.apache.catalina.servlets.FakeCatalinaContext;
 import org.apache.catalina.servlets.FakeCatalinaWebdavResourceRoot;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import de.invesdwin.aspects.annotation.SkipParallelExecution;
 import de.invesdwin.context.integration.webdav.server.WebdavServerProperties;
 import jakarta.servlet.ServletException;
 
+@Configurable
 @Immutable
 public class ConfiguredWebdavServlet extends ExtendedWebdavServlet {
 
@@ -24,6 +30,13 @@ public class ConfiguredWebdavServlet extends ExtendedWebdavServlet {
         showServerInfo = false;
         readOnly = false;
         listings = true;
+    }
+
+    @Override
+    @SkipParallelExecution
+    @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
+    public void periodicEvent() {
+        super.periodicEvent();
     }
 
 }
